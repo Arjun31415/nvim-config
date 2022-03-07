a9d43933f90f7d2b3fc1d242b5959686acafb4c8
Arjun31415 <arjunp0710@gmail.com>
2022-03-07 10:30:10 +0530

    Updates to config

---
+++ lua/config/onedark.lua
@@ -0,0 +1,9 @@
+require('onedark').setup {
+    style = 'darker',
+    colors = {},
+    highlights = {
+        TSStringEscape = {fg = '$cyan'},
+        cppTSVariable = {fg = '$red'},
+        TSFuncMacro = {fg = '$cyan'}
+    }
+}
