8638fa5288def69594aec1b9e1dda3da04b56bd8
Arjun31415 <arjunp0710@gmail.com>
2022-04-07 20:29:30 +0530

    removed some bugs

--- lua/config/lsp/lsp-installer.lua
+++ lua/config/lsp/lsp-installer.lua
@@ -17,7 +17,6 @@ lsp_installer.on_server_ready(function(server)
     end
     if server.name == "sumneko_lua" then
         local sumneko_opts = require("config.lsp.settings.sumneko_lua")
-        vim.pretty_print(sumneko_opts)
         opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
     end
     server:setup(opts)
