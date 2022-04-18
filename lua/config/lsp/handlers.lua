4e0b3480805c9ffe9c740dea5db23d5a7a5c3a3b
Arjun31415 <arjunp0710@gmail.com>
2022-04-18 20:01:46 +0530

    updated lua LSP config to handle breaking changes

--- lua/config/lsp/handlers.lua
+++ lua/config/lsp/handlers.lua
@@ -43,7 +43,7 @@ end
 
 local function lsp_highlight_document(client)
     -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
+    if client.server_capabilities.documentFormattingProvider then
         vim.api.nvim_exec([[
       augroup lsp_document_highlight
         autocmd! * <buffer>
@@ -91,7 +91,7 @@ end
 
 M.on_attach = function(client, bufnr)
     if client.name == "tsserver" then
-        client.resolved_capabilities.document_formatting = false
+        client.server_capabilities.documentFormattingProvider = false
     end
     lsp_keymaps(bufnr)
     lsp_highlight_document(client)
