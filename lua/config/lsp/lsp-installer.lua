2a172a58b91cafe96ff45a7836fb36d35abc462e
Arjun31415 <arjunp0710@gmail.com>
2022-05-10 21:38:24 +0530

    updated `lspconfig` and `lsp-installer`

--- lua/config/lsp/lsp-installer.lua
+++ lua/config/lsp/lsp-installer.lua
@@ -5,23 +5,22 @@ local servers = {
     "tsserver", "cmake", "vimls", "tailwindcss", "cssmodules_ls"
 }
 
-lsp_installer.on_server_ready(function(server)
+local lspconfig = require("lspconfig")
+lsp_installer.setup({})
 
-    local opts = {
-        on_attach = require("config.lsp.handlers").on_attach,
-        capabilities = require("config.lsp.handlers").capabilities
-    }
-    if server.name == "jsonls" then
-        local jsonls_opts = require("config.lsp.settings.jsonls")
-        opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-    end
-    if server.name == "sumneko_lua" then
-        local sumneko_opts = require("config.lsp.settings.sumneko_lua")
-        opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-    end
-    server:setup(opts)
+-- lua language server setup
 
-end)
+local sumneko_opts = require("config.lsp.settings.sumneko_lua")
+local opts = {}
+opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+lspconfig.sumneko_lua.setup(opts)
+
+-- jsonls setup
+
+opts = {}
+local jsonls_opts = require("config.lsp.settings.jsonls")
+opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+lspconfig.jsonls.setup(opts)
 
 for _, name in pairs(servers) do
     local server_is_found, server = lsp_installer.get_server(name)
--- lua/config/lsp/settings/sumneko_lua.lua
+++ lua/config/lsp/settings/sumneko_lua.lua
@@ -1,12 +1,12 @@
 return {
     settings = {
-
         Lua = {
             diagnostics = {globals = {"vim"}},
             workspace = {
                 library = {
-                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.stdpath("config") .. "/lua"] = true
+                    vim.fn.expand("$VIMRUNTIME/lua"),
+                    vim.fn.stdpath("config") .. "/lua",
+                    vim.api.nvim_get_runtime_file("", true)
                 }
             }
         }
