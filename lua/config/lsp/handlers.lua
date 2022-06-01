7c4f8431c9244c94e646792774fa7c2cefd3b9f4
Arjun31415 <arjunp0710@gmail.com>
2022-06-01 08:13:37 +0530

    fixed bugs in document highlighting

--- lua/config/lsp/handlers.lua
+++ lua/config/lsp/handlers.lua
@@ -43,7 +43,7 @@ end
 
 local function lsp_highlight_document(client)
     -- Set autocommands conditional on server_capabilities
-    if client.server_capabilities.documentFormattingProvider then
+    if client.server_capabilities.documentHighlightProvider then
         vim.api.nvim_exec([[
       augroup lsp_document_highlight
         autocmd! * <buffer>
@@ -90,7 +90,7 @@ end
 
 M.on_attach = function(client, bufnr)
     if client.name == "tsserver" then
-        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentHighlightProvider = false
     end
     lsp_keymaps(bufnr)
     lsp_highlight_document(client)
