cca15b1e79dd0ce7b81b34f39a86b3dc414b82ed
Arjun31415 <arjunp0710@gmail.com>
2022-09-09 10:28:26 +0530

    added rust-analyser

--- lua/config/lsp/lsp-installer.lua
+++ lua/config/lsp/lsp-installer.lua
@@ -1,29 +1,48 @@
 local ok, lsp_installer = pcall(require, "mason")
 local ok2, mason_lspconfig = pcall(require, "mason-lspconfig")
 
-if not ok or not ok2 then return end
+if not ok or not ok2 then
+	return
+end
 local servers = {
-    "bashls", "asm_lsp", "pyright", "pylsp", "jsonls", "clangd", "tsserver",
-    "cmake", "vimls", "tailwindcss", "cssmodules_ls"
+	"bashls",
+	"asm_lsp",
+	"pyright",
+	"pylsp",
+	"jsonls",
+	"clangd",
+	"tsserver",
+	"cmake",
+	"vimls",
+	"tailwindcss",
+	"cssmodules_ls",
+	"rust_analyzer",
 }
-mason_lspconfig.setup({ensure_installed = servers})
+mason_lspconfig.setup({ ensure_installed = servers })
 local lsp_server_names = {
-    "bash-language-server", "asm-lsp", "pyright", "python-lsp-server",
-    "json-lsp", "clangd", "typescript-language-server", "cmake-language-server",
-    "vim-language-server", "tailwindcss-language-server",
-    "cssmodules-language-server", "rust_analyzer"
+	"bash-language-server",
+	"pyright",
+	"python-lsp-server",
+	"json-lsp",
+	"clangd",
+	"typescript-language-server",
+	"cmake-language-server",
+	"vim-language-server",
+	"tailwindcss-language-server",
+	"cssmodules-language-server",
+	"rust_analyzer",
 }
 
 local configFunctions = require("config.lsp.handlers")
 local lspconfig = require("lspconfig")
 lsp_installer.setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 -- lua language server setup
@@ -41,46 +60,45 @@ opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 opts.on_attach = configFunctions.on_attach
 lspconfig.jsonls.setup(opts)
 -- r language server
-lspconfig.r_language_server.setup({on_attach = configFunctions.on_attach})
+lspconfig.r_language_server.setup({ on_attach = configFunctions.on_attach })
 -- asm_lsp
-lspconfig.asm_lsp.setup({on_attach = configFunctions.on_attach})
+lspconfig.asm_lsp.setup({ on_attach = configFunctions.on_attach })
 -- bash
-lspconfig.bashls.setup({on_attach = configFunctions.on_attach})
+lspconfig.bashls.setup({ on_attach = configFunctions.on_attach })
 -- cmake setup
-lspconfig.cmake.setup({on_attach = configFunctions.on_attach})
+lspconfig.cmake.setup({ on_attach = configFunctions.on_attach })
 -- clangd setup
-local clangd_capabilities = configFunctions.capabilities;
-clangd_capabilities.offsetEncoding = "utf-8";
+local clangd_capabilities = configFunctions.capabilities
+clangd_capabilities.offsetEncoding = "utf-8"
 lspconfig.clangd.setup({
-    capabilities = clangd_capabilities,
-    on_attach = configFunctions.on_attach
+	capabilities = clangd_capabilities,
+	on_attach = configFunctions.on_attach,
 })
 -- pyright setup
-lspconfig.pyright.setup({on_attach = configFunctions.on_attach})
+lspconfig.pyright.setup({ on_attach = configFunctions.on_attach })
 -- pylsp
-lspconfig.pylsp.setup({on_attach = configFunctions.on_attach})
+lspconfig.pylsp.setup({ on_attach = configFunctions.on_attach })
 -- tssserver
-lspconfig.tsserver.setup({on_attach = configFunctions.on_attach})
+lspconfig.tsserver.setup({ on_attach = configFunctions.on_attach })
 -- eslint server
-lspconfig.eslint.setup({on_attach = configFunctions.on_attach})
+lspconfig.eslint.setup({ on_attach = configFunctions.on_attach })
 -- html server
-lspconfig.html.setup({on_attach = configFunctions.on_attach})
+lspconfig.html.setup({ on_attach = configFunctions.on_attach })
 -- svelete config
-lspconfig.svelte.setup({on_attach = configFunctions.on_attach})
+lspconfig.svelte.setup({ on_attach = configFunctions.on_attach })
 -- css server
-lspconfig.cssmodules_ls.setup({on_attach = configFunctions.on_attach})
+lspconfig.cssmodules_ls.setup({ on_attach = configFunctions.on_attach })
 -- tailwindcss
-lspconfig.tailwindcss.setup({on_attach = configFunctions.on_attach})
+lspconfig.tailwindcss.setup({ on_attach = configFunctions.on_attach })
 -- vim
-lspconfig.vimls.setup({on_attach = configFunctions.on_attach})
+lspconfig.vimls.setup({ on_attach = configFunctions.on_attach })
 -- rust_analyzer
 require("config.lsp.settings.rust")
 -- lspconfig.rust_analyzer.setup({on_attach = configFunctions.on_attach})
 -- latex server
-lspconfig.texlab.setup({on_attach = configFunctions.on_attach})
+lspconfig.texlab.setup({ on_attach = configFunctions.on_attach })
 vim.api.nvim_create_user_command("MasonInstallAll", function()
-    for _, name in pairs(lsp_server_names) do
-        vim.cmd("MasonInstall " .. name)
-    end
-
+	for _, name in pairs(lsp_server_names) do
+		vim.cmd("MasonInstall " .. name)
+	end
 end, {})
