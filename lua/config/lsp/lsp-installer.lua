468575c77aee6aefbebd3d9fbaf7c62a2c3b8039
Arjun31415 <arjunp0710@gmail.com>
2023-08-25 09:00:21 +0530

    pyright config

--- lua/config/lsp/lsp-installer.lua
+++ lua/config/lsp/lsp-installer.lua
@@ -30,13 +30,17 @@ require("lspconfig").clangd.setup({
   capabilities = clangd_capabilities,
   on_attach = configFunctions.on_attach,
 })
+require("lspconfig").pyright.setup({
+  capabilities = configFunctions.capabilities,
+  on_attach = configFunctions.on_attach,
+})
 local jsonls_opts = require("config.lsp.settings.jsonls")
 local opts = {}
 opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 opts.on_attach = configFunctions.on_attach
 require("lspconfig").jsonls.setup(opts)
 local sumneko_opts = require("config.lsp.settings.sumneko_lua")
-local opts = {}
+opts = {}
 opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 opts.on_attach = configFunctions.on_attach
 
