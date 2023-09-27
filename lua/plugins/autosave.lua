1e4432e26781226d4f246300696bcc4bf25669a7
Arjun31415 <arjunp0710@gmail.com>
2023-09-27 14:34:25 +0530

    added auto-save.nvim

---
+++ lua/plugins/autosave.lua
@@ -0,0 +1,27 @@
+return {
+    "Pocco81/auto-save.nvim",
+    event = "VimEnter",
+    config = function()
+        require("auto-save").setup({
+            enabled = true,
+            execution_message = {
+                message = function() -- message to print on save
+                    return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+                end,
+                dim = 0.18, -- dim the color of `message`
+                cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+            },
+            trigger_events = { "InsertLeave", "TextChanged" },
+            conditions = {
+                exists = true,
+                filename_is_not = { "plugins.lua" },
+                filetype_is_not = {},
+                modifiable = true,
+            },
+            write_all_buffers = false,
+            on_off_commands = true,
+            clean_command_line_interval = 1000,
+            debounce_delay = 135,
+        })
+    end,
+}
