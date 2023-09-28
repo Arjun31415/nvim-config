dc145eeec96790b9a98a637664405ffd38be828c
Arjun31415 <arjunp0710@gmail.com>
2023-09-28 11:22:09 +0530

    updated indent blankline

--- init.lua
+++ init.lua
@@ -114,15 +114,50 @@ require("lazy").setup({
         "lukas-reineke/indent-blankline.nvim",
         -- Enable `lukas-reineke/indent-blankline.nvim`
         -- See `:help indent_blankline.txt`
-        opts = {
+        config = function()
             -- U+2502 may also be a good choice, it will be on the middle of cursor.
             -- U+250A is also a good choice
-            char = "|",
-            show_trailing_blankline_indent = false,
-            show_current_context = true,
-            show_current_context_start = true,
-        },
-        show_current_context = false,
+            --  v2 config
+            -- char = "|",
+            -- show_trailing_blankline_indent = false,
+            -- show_current_context = true,
+            -- show_current_context_start = true,
+            -- v3 config
+            local highlight = {
+                "RainbowRed",
+                "RainbowYellow",
+                "RainbowBlue",
+                "RainbowOrange",
+                "RainbowGreen",
+                "RainbowViolet",
+                "RainbowCyan",
+            }
+
+            local hooks = require("ibl.hooks")
+            -- create the highlight groups in the highlight setup hook, so they are reset
+            -- every time the colorscheme changes
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
+
+            require("ibl").setup({
+                indent = {
+                    smart_indent_cap = true,
+                },
+                scope = {
+                    include = { lua = { "return_statement", "table_constructor" } },
+                    enabled = true,
+                    priority = 500,
+                    highlight = highlight,
+                },
+            })
+        end,
     },
     --
     -- "gc" to comment visual regions/lines
