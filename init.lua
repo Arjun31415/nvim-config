cad7dcd84b4852e7f7c05fce25fc690f72285c91
Arjun31415 <arjunp0710@gmail.com>
2024-06-14 21:39:06 +0530

    added mini.pairs and mini.surround

--- init.lua
+++ init.lua
@@ -226,7 +226,51 @@ require("lazy").setup({
         },
         build = ":TSUpdate",
     },
-    { "echasnovski/mini.ai", version = false },
+    {
+        "echasnovski/mini.ai",
+        version = false,
+        config = function()
+            require("mini.ai").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.pairs",
+        version = false,
+        config = function()
+            require("mini.pairs").setup({
+                -- In which modes mappings from this `config` should be created
+                modes = { insert = true, command = false, terminal = false },
+
+                -- Global mappings. Each right hand side should be a pair information, a
+                -- table with at least these fields (see more in |MiniPairs.map|):
+                -- - <action> - one of 'open', 'close', 'closeopen'.
+                -- - <pair> - two character string for pair to be used.
+                -- By default pair is not inserted after `\`, quotes are not recognized by
+                -- `<CR>`, `'` does not insert pair after a letter.
+                -- Only parts of tables can be tweaked (others will use these defaults).
+                mappings = {
+                    ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+                    ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+                    ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+
+                    [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+                    ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+                    ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+                    ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+                    ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+                    ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+                },
+            })
+        end,
+    },
+    {
+        "echasnovski/mini.surround",
+        version = false,
+        config = function()
+            require("mini.surround").setup()
+        end,
+    },
 }, {})
 
 -- [[ Setting options ]]
