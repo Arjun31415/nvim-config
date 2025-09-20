9eb42ae369d9d3802169eb9c7d929e0c71b4fbd7
Arjun31415 <arjunp0710@gmail.com>
2025-09-21 01:31:51 +0200

    updated lsp config

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -1,5 +1,5 @@
 local configFunctions = require("config.lsp.handlers")
-local lspconfig = require("lspconfig")
+local lspconfig = vim.lsp.config
 local servers = {
     "basedpyright",
     "clangd",
@@ -9,10 +9,11 @@ local servers = {
     -- "nixd",
 }
 for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup({
+    lspconfig(lsp, {
         capabilities = configFunctions.capabilities,
         on_attach = configFunctions.on_attach,
     })
+    vim.lsp.enable(lsp)
 end
 require("config.lsp.settings.rust")
 
@@ -20,8 +21,19 @@ local jsonls_opts = require("config.lsp.settings.jsonls")
 local opts = {}
 opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
 opts.on_attach = configFunctions.on_attach
-lspconfig.jsonls.setup(opts)
-lspconfig.lua_ls.setup({
+-- lspconfig.jsonls.setup(opts)
+lspconfig("jsonls", opts)
+vim.lsp.enable("jsonls")
+-- lspconfig.lua_ls.setup({
+--     capabilities = configFunctions.capabilities,
+--     on_attach = configFunctions.on_attach,
+--     settings = {
+--         Lua = {
+--             workspace = { checkThirdParty = false },
+--         },
+--     },
+-- })
+lspconfig("lua_ls", {
     capabilities = configFunctions.capabilities,
     on_attach = configFunctions.on_attach,
     settings = {
@@ -30,3 +42,4 @@ lspconfig.lua_ls.setup({
         },
     },
 })
+vim.lsp.enable("lua_ls")
