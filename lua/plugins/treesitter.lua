e86d8d5bd1967c1b5febefb072ae5d4450852d07
Arjun31415 <arjunp0710@gmail.com>
2026-04-08 18:37:05 +0200

    update to treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,97 +1,201 @@
 return {
     {
-        -- Highlight, edit, and navigate code
         "nvim-treesitter/nvim-treesitter",
         dependencies = {
-            "tree-sitter-grammars/tree-sitter-hyprlang",
-            "nvim-treesitter/nvim-treesitter-textobjects",
+            {
+                "nvim-treesitter/nvim-treesitter-context",
+                opts = {
+                    max_lines = 4,
+                    multiline_threshold = 2,
+                },
+            },
         },
+        lazy = false,
+        branch = "main",
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
-                -- Add languages to be installed here that you want installed for treesitter
-                ensure_installed = {
-                    "c",
-                    "cpp",
-                    "lua",
-                    "python",
-                    "rust",
-                    "vimdoc",
-                    "vim",
-                    "regex",
-                    "markdown",
-                    "markdown_inline",
-                },
+            local ts = require("nvim-treesitter")
 
-                -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-                auto_install = true,
+            -- State tracking for async parser loading
+            local parsers_loaded = {}
+            local parsers_pending = {}
+            local parsers_failed = {}
 
-                highlight = { enable = true },
-                indent = { enable = true },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<c-space>",
-                        node_incremental = "<c-space>",
-                        scope_incremental = "<c-s>",
-                        node_decremental = "<M-space>",
+            local ns = vim.api.nvim_create_namespace("treesitter.async")
+
+            -- Helper to start highlighting and indentation
+            local function start(buf, lang)
+                local ok = pcall(vim.treesitter.start, buf, lang)
+                if ok then
+                    vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+                return ok
+            end
+
+            -- Install core parsers after lazy.nvim finishes loading all plugins
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "LazyDone",
+                once = true,
+                callback = function()
+                    ts.install({
+                        "bash",
+                        "comment",
+                        "c",
+                        "cpp",
+                        "diff",
+                        "fish",
+                        "git_config",
+                        "git_rebase",
+                        "gitcommit",
+                        "gitignore",
+                        "json",
+                        "latex",
+                        "lua",
+                        "luadoc",
+                        "make",
+                        "markdown",
+                        "markdown_inline",
+                        "python",
+                        "query",
+                        "regex",
+                        "rust",
+                        "toml",
+                        "typst",
+                        "vim",
+                        "vimdoc",
+                    }, {
+                        max_jobs = 8,
+                    })
+                end,
+            })
+
+            -- Decoration provider for async parser loading
+            vim.api.nvim_set_decoration_provider(ns, {
+                on_start = vim.schedule_wrap(function()
+                    if #parsers_pending == 0 then
+                        return false
+                    end
+                    for _, data in ipairs(parsers_pending) do
+                        if vim.api.nvim_buf_is_valid(data.buf) then
+                            if start(data.buf, data.lang) then
+                                parsers_loaded[data.lang] = true
+                            else
+                                parsers_failed[data.lang] = true
+                            end
+                        end
+                    end
+                    parsers_pending = {}
+                end),
+            })
+
+            local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
+
+            local ignore_filetypes = {
+                "checkhealth",
+                "lazy",
+                "mason",
+                "snacks_dashboard",
+                "snacks_notif",
+                "snacks_win",
+                "noice",
+                "nvim-tree",
+                "neotree",
+                "trouble",
+            }
+
+            -- Auto-install parsers and enable highlighting on FileType
+            vim.api.nvim_create_autocmd("FileType", {
+                group = group,
+                desc = "Enable treesitter highlighting and indentation (non-blocking)",
+                callback = function(event)
+                    if vim.tbl_contains(ignore_filetypes, event.match) then
+                        return
+                    end
+
+                    local lang = vim.treesitter.language.get_lang(event.match) or event.match
+                    local buf = event.buf
+
+                    if parsers_failed[lang] then
+                        return
+                    end
+
+                    if parsers_loaded[lang] then
+                        -- Parser already loaded, start immediately (fast path)
+                        start(buf, lang)
+                    else
+                        -- Queue for async loading
+                        table.insert(parsers_pending, { buf = buf, lang = lang })
+                    end
+
+                    -- Auto-install missing parsers (async, no-op if already installed)
+                    ts.install({ lang })
+                end,
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        init = function()
+            -- Disable entire built-in ftplugin mappings to avoid conflicts.
+            -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+            vim.g.no_plugin_maps = true
+
+            -- Or, disable per filetype (add as you like)
+            -- vim.g.no_python_maps = true
+            -- vim.g.no_ruby_maps = true
+            -- vim.g.no_rust_maps = true
+            -- vim.g.no_go_maps = true
+        end,
+        config = function()
+            ---- configuration
+            require("nvim-treesitter-textobjects").setup({
+                select = {
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    selection_modes = {
+                        ["@parameter.outer"] = "v", -- charwise
+                        ["@function.outer"] = "V", -- linewise
+                        -- ['@class.outer'] = '<c-v>', -- blockwise
                     },
-                },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["aa"] = "@parameter.outer",
-                            ["ia"] = "@parameter.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            ["]m"] = "@function.outer",
-                            ["]]"] = "@class.outer",
-                        },
-                        goto_next_end = {
-                            ["]M"] = "@function.outer",
-                            ["]["] = "@class.outer",
-                        },
-                        goto_previous_start = {
-                            ["[m"] = "@function.outer",
-                            ["[["] = "@class.outer",
-                        },
-                        goto_previous_end = {
-                            ["[M"] = "@function.outer",
-                            ["[]"] = "@class.outer",
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ["<leader>a"] = "@parameter.inner",
-                        },
-                        swap_previous = {
-                            ["<leader>A"] = "@parameter.inner",
-                        },
-                    },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true of false
+                    include_surrounding_whitespace = false,
                 },
             })
 
-            local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-            parser_config.hypr = {
-                install_info = {
-                    url = "https://github.com/luckasRanarison/tree-sitter-hyprlang",
-                    files = { "src/parser.c" },
-                    branch = "master",
-                },
-                filetype = "hypr",
-            }
+            -- keymaps
+            -- You can use the capture groups defined in `textobjects.scm`
+            vim.keymap.set({ "x", "o" }, "am", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+            end, { desc = "select function outer" })
+            vim.keymap.set({ "x", "o" }, "im", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+            end, { desc = "select function inner" })
+            vim.keymap.set({ "x", "o" }, "ac", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+            end, { desc = "select class outer" })
+            vim.keymap.set({ "x", "o" }, "ic", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+            end, { desc = "select class inner" })
+            -- You can also use captures from other query groups like `locals.scm`
+            vim.keymap.set({ "x", "o" }, "as", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
+            end, { desc = "select local scope" })
         end,
     },
 }
