08d02b8c974b81d394d796b0a42e96fe7ba8a298
Arjun31415 <arjunp0710@gmail.com>
2026-09-20 11:22:09 +1000

    Fix bug in neotree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -48,12 +48,8 @@ return {
         "MunifTanjim/nui.nvim",
     },
     opts = {
-        use_libuv_file_watcher = true,
         enable_git_status = true,
         enable_diagnostics = true,
-        disable_netrw = true,
-
-        follow_current_file = { enabled = true },
         window = {
             mapping_options = {
                 noremap = true,
@@ -87,18 +83,19 @@ return {
                 staged = "",
                 conflict = "",
             },
-            filesystem = {
-                disable_netrw = true,
-
-                hijack_netrw_behavior = "open_current",
-                commands = {
-                    -- Override delete to use trash instead of rm
-                    delete = function(state)
-                        local path = state.tree:get_node().path
-                        vim.fn.system({ "trash", vim.fn.fnameescape(path) })
-                        require("neo-tree.sources.manager").refresh(state.name)
-                    end,
-                },
+        },
+        filesystem = {
+            disable_netrw = true,
+            hijack_netrw_behavior = "open_current",
+            use_libuv_file_watcher = true,
+            follow_current_file = { enabled = true },
+            commands = {
+                -- Override delete to use trash instead of rm
+                delete = function(state)
+                    local path = state.tree:get_node().path
+                    vim.fn.system({ "trash", vim.fn.fnameescape(path) })
+                    require("neo-tree.sources.manager").refresh(state.name)
+                end,
             },
         },
     },
